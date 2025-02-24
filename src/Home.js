5f088b9e622a007c5f3a07e54dc51b72cea05653
Phanindra <phanindragadireddy2@gmail.com>
2025-02-24 16:36:41 +0530

    styles updated

--- src/Home.js
+++ src/Home.js
@@ -24,7 +24,7 @@ function Home() {
             <FiSearch className='search-icon' />
             <input
               type='text'
-              placeholder='Search for free images, videos, music & more'
+              placeholder='Search for free images & more unique images'
               value={search}
               onChange={(e) => { setSearch(e.target.value) }}
               className='search-input'
