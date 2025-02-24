5f81ada5c2d22d6ceb5fe48e8100e72c223556ce
Phanindra <phanindragadireddy2@gmail.com>
2025-02-24 16:27:50 +0530

    otp form created using shadcn

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import {useNavigate} from 'react-router'
+import { useNavigate } from 'react-router'
 import { FiSearch } from 'react-icons/fi';
 import './App.css'
 
 function Home() {
-    let navigator = useNavigate()
-    const [search, setSearch] = useState('')
+  let navigator = useNavigate()
+  const [search, setSearch] = useState('')
 
   return (
     <div className='bgimage'>
-      <div>
+      <div className='header-container'>
         <h2 className='brandname'>Pixabay</h2>
         <div className='authentication'>
-            <button>Login</button> 
-            <button>Sign Up</button> 
+          <button className='auth-button'>Login</button>
+          <button className='auth-button'>Sign Up</button>
         </div>
       </div>
-      <div id='hero' >
-        <h1 className='header1'>Stunning royalty-free images & royalty-free stock</h1>
+      <div id='hero'>
+        <h1 className='header1'>Stunning royalty-free images & stock</h1>
         <h2 className='header2'>Over 5.2 million+ high quality stock images, videos and music shared by our talented community.</h2>
         <div className='form'>
-        <FiSearch className='search-icon' style={{ marginRight: '0px', color: '#aaa', fontSize: '30px' }} />
-        <input type='text' placeholder='Search for free images, Videos, Music & more' value={search} onChange={(e) => {setSearch(e.target.value)} } /> <br></br>
-        <br></br><button className='search' onClick={() => { search && navigator(`/search/${search}`)}}>Search</button>
+          <div className='search-container'>
+            <FiSearch className='search-icon' />
+            <input
+              type='text'
+              placeholder='Search for free images, videos, music & more'
+              value={search}
+              onChange={(e) => { setSearch(e.target.value) }}
+              className='search-input'
+            />
+          </div>
+          <button className='search' onClick={() => { search && navigator(`/search/${search}`) }}>Search</button>
         </div>
       </div>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -2,36 +2,42 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './Search.css'
+import { FiSearch } from 'react-icons/fi'
 
-function Search({match}) {
+function Search() {
     const { search } = useParams()
-    const [data,setData] = useState([])
-    useEffect(() =>{
-        axios.get(`https://pixabay.com/api/?key=48179794-e08dd0f528dab542905c968d2&q=${search}&image_type=photo&pretty=true`).then(res => setData(res.data.hits))
-    },[])
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://pixabay.com/api/?key=48179794-e08dd0f528dab542905c968d2&q=${search}&image_type=photo&pretty=true`)
+            .then(res => setData(res.data.hits))
+    }, [search])
 
     return (
-      <div className='searchs'>
-        <div className='row'>
-            {data && data.map(data => <div className='col-md-4'>
-                <div className='card'>
-                <img src={data.webformatURL} alt={data.user} className='img-fluid' height='300px' />
-                <div className='card-body'>
-                    <h4 className='card-title'>{data.tags}</h4>
-                </div>
-                <div className='card-footer'>
-                <h5>Info</h5>
-                <p>Views : {data.views} views</p>
-                <p>Downloads : {data.downloads} downloads</p>
-                <p>Likes : {data.likes} likes</p>
-                <p>Comments : {data.comments} comments</p>
-                <p> URL : {data.pageURL}</p>
-                </div>
-  
-                </div>
-            </div>)}
+        <div className='search-container'>
+            <h2 className='search-title'>Results for: {search}</h2>
+            <div className='row'>
+                {data && data.map(item => (
+                    <div className='col-md-4 col-sm-6 mb-4' key={item.id}>
+                        <div className='card'>
+                            <img src={item.webformatURL} alt={item.user} className='img-fluid' />
+                            <div className='card-body'>
+                                <h4 className='card-title'>{item.tags}</h4>
+                            </div>
+                            <div className='card-footer'>
+                                <h5>Info</h5>
+                                <p>Views: {item.views}</p>
+                                <p>Downloads: {item.downloads}</p>
+                                <p>Likes: {item.likes}</p>
+                                <p>Comments: {item.comments}</p>
+                                <p>URL: <a href={item.pageURL} target="_blank" rel="noopener noreferrer">{item.pageURL}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
     )
 }
 
